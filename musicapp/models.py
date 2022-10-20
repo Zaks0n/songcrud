cea2be1864b1bf46f7ff2f979b68f7049922a1bf
Zakariya Abdulhameed <zaksontech@gmail.com>
2022-10-20 12:33:25 +0100

    Added Models

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.lyric
